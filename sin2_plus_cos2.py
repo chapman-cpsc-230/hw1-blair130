6c94449f50b4d9a64fe782eb028edaa1957cb1b2
Lucas Blair <blair130@mail.chapman.edu>
2016-02-18 15:46:53 -0800

    Update sin2_plus_cos2.py

--- sin2_plus_cos2.py
+++ sin2_plus_cos2.py
@@ -1,3 +1,12 @@
+"""
+File: <sin2_plus_cos2.py>
+
+Copyright (c) 2016 <Lucas Blair>
+
+License: MIT
+
+<Calculates a sin and cos formula accurately>
+"""
 from math import sin, cos, pi
 x = pi/4
 sumSquares = (sin(x) * sin(x)) + (cos(x) * cos(x))
